420f2cafcd6ec34c97365a071c089733827bd85b
Jedidiah Cheng <chengjedidiah@gmail.com>
2025-12-06 09:11:56 +0800

    solve day 5

--- 5.py
+++ 5.py
@@ -1,5 +1,31 @@
 import sys
 
+# We want to process the ranges, merging them into con
+def count_fresh_ingredients(ranges: list[tuple[int, int]]):
+    # sort ranges by their start id.
+    sorted_ranges = list(sorted(ranges, key=lambda r: r[0]))
+    count = 0
+    last = -1
+
+    for start, end in sorted_ranges:
+        # If the new range ends before the merged range, it completely overlaps with the merged range.
+        # this works because the ranges are sorted by their start ids.
+        if end <= last:
+            continue
+
+        # Here, the new range overlaps with the merged range so, we "extend" the merged range.
+        if start <= last:
+            count += end - last
+        # Otherwise, the new range does not overlap, so we simply add the ids in the new range.
+        else:
+            count += end - start + 1
+
+        last = max(end, last)
+
+    return count
+
+
+
 def solve(ranges: list[tuple[int, int]], available: list[int]) -> int:
     # First, we pre-process the ranges into an ordered dict where the keys are the start of a range.
     # If the start of the range already exists, if the new range is within the old range,
@@ -17,7 +43,9 @@ def solve(ranges: list[tuple[int, int]], available: list[int]) -> int:
                 part_one += 1
                 break
 
-    return part_one
+    part_two = count_fresh_ingredients(ranges)
+
+    return part_one, part_two
 
 def parse_input():
     ranges = []
@@ -30,7 +58,7 @@ def parse_input():
             continue
 
         if is_processing_ranges:
-            ranges.append(list(map(int, line.split('-'))))
+            ranges.append(tuple(map(int, line.split('-'))))
         else:
             available_ingredients.append(int(line.strip()))
 
