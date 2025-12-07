eab38d55a5f3ddc114bcfea97f14aaf6452ee692
Jedidiah Cheng <chengjedidiah@gmail.com>
2025-12-07 23:17:10 +0800

    solve day 7 part 2

--- 7.py
+++ 7.py
@@ -29,9 +29,37 @@ def solve_part_one(starting_pos: tuple[int, int], boundary: tuple[int, int], spl
     return part_one
 
 # We need to count the total number of unique paths taken by the beam.
-# Recursion: 2^n possible
-# def solve_part_two():
+# Recursion: 2^splitters per row. 2 choices (left, right). If we brute force, we need to count 2^n.
 
+# DP: we sum up number of ways to reach each x_position on the last row.
+def solve_part_two(starting_pos: tuple[int, int], boundary: tuple[int, int], splitters: dict[int, set[int]]):
+    current_row = starting_pos[1] + 1
+    beams = {starting_pos[0]: 1}
+
+    while current_row < boundary[1]:
+        next_beams = {}
+
+        for beam_x in beams:
+            # If the beam encounters a splitter, split.
+            if current_row in splitters and beam_x in splitters[current_row]:
+                for next_beam_x in [beam_x - 1, beam_x + 1]:
+                    if 0 <= next_beam_x < boundary[0]:
+                        if next_beam_x not in next_beams:
+                            next_beams[next_beam_x] = 0
+
+                        next_beams[next_beam_x] += beams[beam_x]
+
+            # If the beam did not split, continue.
+            else:
+                if beam_x not in next_beams:
+                    next_beams[beam_x] = 0
+
+                next_beams[beam_x] += beams[beam_x]
+
+        beams = next_beams
+        current_row += 1
+
+    return sum(beams.values())
 
 def parse_input():
     starting_pos = None
@@ -68,7 +96,9 @@ def parse_input():
 
 def main():
     starting_pos, boundary, splitters = parse_input()
-    result = solve_part_one(starting_pos, boundary, splitters)
-    print(result)
+    part_one = solve_part_one(starting_pos, boundary, splitters)
+    part_two = solve_part_two(starting_pos, boundary, splitters)
+
+    print(part_one, part_two)
 
 main()
