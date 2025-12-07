e9745ecae67b44896b303c121dba5847d9a83a97
Jedidiah Cheng <chengjedidiah@gmail.com>
2025-12-07 10:33:47 +0800

    solve day 6 part 2

--- 6.py
+++ 6.py
@@ -1,5 +1,4 @@
 import sys
-from unittest import result
 
 def solve(operands_list: list[list[int]], operators: list[str]):
     part_one = 0
@@ -15,20 +14,20 @@ def solve(operands_list: list[list[int]], operators: list[str]):
 
         part_one += result
 
-    part_two = 0
-    # We need to interpret the operands differently in this case.
-    # Re-process operands to the new format.
-
-
     return part_one
 
 def parse_input():
     operands_list = []
     operators = []
+
+    lines = []
+
+    max_line_length = 0
     for line in sys.stdin:
         if line.strip() == '':
             break
 
+        # Parse part_one input
         for index, element in enumerate(line.split()):
             # Add operator
             if element == '*' or element == '+':
@@ -41,11 +40,38 @@ def parse_input():
 
             operands_list[index].append(int(element))
 
-    return operands_list, operators
+        # Parse part_two input: We store all elements in an array of strings first.
+        max_line_length = max(max_line_length, len(line[:-1]))
+        lines.append(list(line[:-1]))
+
+    # Now we process the previously parsed part_two input, using the operand position as a guideline.
+    # lines[-1] should be the row with the operands.
+    part_two_operands = []
+    current_set = -1
+    for index in range(max_line_length):
+        if index < len(lines[-1]):
+            char = lines[-1][index]
+            if char == '*' or char == '+':
+                part_two_operands.append([])
+                current_set += 1
+
+        current = 0
+        for row in range(len(lines) - 1):
+            if index >= len(lines[row]) or lines[row][index] == ' ':
+                continue
+
+            current *= 10
+            current += int(lines[row][index])
+
+        if current != 0:
+            part_two_operands[current_set].append(current)
+
+    return operands_list, part_two_operands, operators
 
 def main():
-    operands_list, operators = parse_input()
-    result = solve(operands_list, operators)
-    print(result)
+    operands_list, operands_list_p2, operators = parse_input()
+    part_one = solve(operands_list, operators)
+    part_two = solve(operands_list_p2, operators)
+    print(part_one, part_two)
 
 main()
